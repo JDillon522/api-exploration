efd262e412041661e45a3d10a524f9b5b1a926fe
Dillon, Joseph <joseph.dillon.522@gmail.com>
2018-11-23 22:35:21 -0500

    I guess not crashing is a good start

--- REST.MVC/Startup.cs
+++ REST.MVC/Startup.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using API.Authentication.Custom;
+using API.Data.Entities;
+using API.Data.Models;
+using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -14,12 +21,12 @@ namespace REST.MVC
 {
     public class Startup
     {
+        public static IConfiguration Configuration;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -32,7 +39,45 @@ namespace REST.MVC
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc();
+
+            string identityConnectionString = Startup.Configuration["connectionStrings:LocalDbIdentity"];
+            string migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+            services.AddDbContext<UserDbContext>(options => {
+                options.UseSqlServer(identityConnectionString, sql => {
+                    sql.MigrationsAssembly("API.Data");
+                });
+            });
+
+            services.AddIdentity<UserModel, IdentityRole>(options => {
+                    options.SignIn.RequireConfirmedEmail = true;
+                    options.Tokens.EmailConfirmationTokenProvider = "emailConfirm";
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequiredUniqueChars = 5;
+                    options.Password.RequiredLength = 15;
+
+                    options.User.RequireUniqueEmail = true;
+                })
+                .AddEntityFrameworkStores<UserDbContext>()
+                .AddDefaultTokenProviders()
+                .AddTokenProvider<EmailConfirmationTokenProvider<UserModel>>("emailConfirm")
+                .AddPasswordValidator<CustomPasswordValidator<UserModel>>();
+
+            services.Configure<DataProtectionTokenProviderOptions>(options => {
+                options.TokenLifespan = TimeSpan.FromHours(3);
+            });
+            services.Configure<EmailConfirmationTokenProviderOptions>(options => {
+                options.TokenLifespan = TimeSpan.FromDays(2);
+            });
+            services.Configure<PasswordHasherOptions>(options => {
+                options.IterationCount = 100000;
+            });
+            services.AddScoped<IUserStore<UserModel>, UserOnlyStore<UserModel, UserDbContext>>();
+
+            services.ConfigureApplicationCookie(options => options.LoginPath = "/api/login");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,7 +96,7 @@ namespace REST.MVC
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-
+            app.UseAuthentication();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(

2be4257854b95a5ac95cec336209a52e66e22020
Dillon, Joseph <joseph.dillon.522@gmail.com>
2018-11-23 21:27:14 -0500

    giving up on trying to run multiple MVC apps from one server (mvc) at the same time. *sigh*

--- API.Authentication/Controllers/LoginController.cs
+++ API.Authentication/Controllers/LoginController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Authentication.Controllers
@@ -5,6 +6,14 @@ namespace API.Authentication.Controllers
     [Route("login")]
     public class LoginController : Controller
     {
+
+        private IHostingEnvironment _hostingEnvironment { get; }
+
+        public LoginController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
         [HttpGet("")]
         public IActionResult Index()
         {
--- Shared.Config/StartupConfig.cs
+++ Shared.Config/StartupConfig.cs
@@ -17,24 +17,24 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Server
+namespace Shared.Config
 {
-    public class Startup
+    public class StartupConfig
     {
         public static IConfiguration Configuration;
 
-        public Startup(IConfiguration configuration) {
+        public StartupConfig(IConfiguration configuration) {
             Configuration = configuration;
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceCollection AddConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
 
-            string identityConnectionString = Startup.Configuration["connectionStrings:LocalDbIdentity"];
-            string migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+            string identityConnectionString = StartupConfig.Configuration["connectionStrings:LocalDbIdentity"];
+            string migrationsAssembly = typeof(StartupConfig).GetTypeInfo().Assembly.GetName().Name;
             services.AddDbContext<UserDbContext>(options => {
                 options.UseSqlServer(identityConnectionString, sql => {
                     sql.MigrationsAssembly("API.Data");
@@ -72,21 +72,26 @@ namespace Server
             services.ConfigureApplicationCookie(options => options.LoginPath = "/api/login");
 
             services.Configure<RazorViewEngineOptions>(options => {
-                options.ViewLocationFormats.Clear();
-                options.ViewLocationFormats.Add("~/API.Authentication/Views/{1}/{0}" + RazorViewEngine.ViewExtension);
+                // options.ViewLocationExpanders.Add(new ViewLocationExpander());
+                // options.AreaViewLocationFormats.Clear();
+                // options.AreaViewLocationFormats.Add("/API.Authentication/Views/{1}/{0}" + RazorViewEngine.ViewExtension);
             });
+
+            return services;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IAntiforgery antiforgery, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+        // public void Configure(IApplicationBuilder app, IAntiforgery antiforgery, IHostingEnvironment env)
+        // {
+        //     if (env.IsDevelopment())
+        //     {
+        //         app.UseDeveloperExceptionPage();
+        //     }
 
-            app.UseAuthentication();
-            app.UseMvc();
-        }
+        //     app.UseAuthentication();
+
+
+        //     app.UseMvc();
+        // }
     }
 }

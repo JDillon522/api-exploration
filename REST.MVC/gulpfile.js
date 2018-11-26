85dc1f2b7cc92b808d5a6c0cc2042f19618f3a8d
Dillon, Joseph <joseph.dillon.522@gmail.com>
2018-11-26 09:57:52 -0500

    moved sass to same folder as views. switched to bootstrap-material. fixed stupid tilde issue in nodemodule imports

--- REST.MVC/gulpfile.js
+++ REST.MVC/gulpfile.js
@@ -2,30 +2,41 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const tildeImporter = require('node-sass-tilde-importer');
+
 sass.compiler = require('node-sass');
-const rimraf = require('rimraf');
+const sassOptions = {
+    importer: tildeImporter
+}
 
 const paths = {
-    webroot: './wwwroot/'
+    scssRoot: './Views/',
+    outputRoot: './wwwroot/'
 };
 
-paths.stylesRoot = paths.webroot + 'scss/site.scss';
-paths.styles = paths.webroot + 'scss/**/*.scss';
-paths.stylesDest = paths.webroot + 'css';
+paths.stylesRoot = paths.scssRoot + 'site.scss';
+paths.styles = paths.scssRoot + '**/*.scss';
+paths.stylesDest = paths.outputRoot + 'css';
 
 gulp.task('scss', () => {
-    console.log('Updating Scss...');
-
     return gulp.src(paths.stylesRoot)
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass(sassOptions).on('error', sass.logError))
         .pipe(gulp.dest(paths.stylesDest));
 });
+
+gulp.task('autoprefixer', () =>{
+    return gulp.src(paths.stylesDest)
+        .pipe(autoprefixer({}))
+        .pipe(gulp.dest('dist'))
+    }
+);
 
 gulp.task('scss:watch', () => {
     console.log(`Watching files at: ${paths.styles}`);
 
-    return gulp.watch(paths.styles, gulp.series(['scss']));
+    return gulp.watch(paths.styles, gulp.series(['scss', 'autoprefixer']));
 });
 
 // A 'default' task is required by Gulp v4
-gulp.task('default', gulp.series(['scss', 'scss:watch']));
+gulp.task('default', gulp.series(['scss', 'autoprefixer', 'scss:watch']));
